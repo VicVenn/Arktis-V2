1a7610b2ec53acba9e8587662b8b542075fe96d4
VicVenn <victor.vennerstroem@gmail.com>
2018-04-10 12:50:28 +0200

    Restart

--- Source/loader.js
+++ Source/loader.js
@@ -1,4 +1,28 @@
-window.addEventListener("load", function(){
-	var bajs = document.getElementById("bajs");
-	document.body.removeChild(bajs);
+window.addEventListener("load", function() {
+  var loader = document.getElementById("loader");
+  document.body.removeChild(loader);
 });
+
+$(document).ready(function() {
+  $('a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $('html, body').animate({
+      'scrollTop': $target.offset().top
+    }, 1000, 'swing', function() {
+      window.location.hash = target;
+    });
+  });
+});
+
+function myFunction() {
+		var x = document.getElementById("myTopnav");
+		if (x.className === "topnav") {
+				x.className += " responsive";
+		} else {
+				x.className = "topnav";
+		}
+}
